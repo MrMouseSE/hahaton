7c5379e6f25f426c8bab9c6b20528341573095de
Danil Bogodvid <d.bogodvid@kefirgames.com>
2020-02-22 12:59:43 +0400

    -add: controllers;

--- Assets/fckingCODE/TowerContainer.cs
+++ Assets/fckingCODE/TowerContainer.cs
@@ -1,9 +1,15 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace fckingCODE
 {
     public class TowerContainer : MonoBehaviour
     {
+        public GameObject Bullet;
+        public float BulletSpeed;
         
+        [NonSerialized]
+        public EnemySpawner EnemySpawner;
+
     }
 }
--- Assets/fckingCODE/TowerController.cs
+++ Assets/fckingCODE/TowerController.cs
@@ -5,7 +5,55 @@ namespace fckingCODE
     public class TowerController : MonoBehaviour
     {
         public TowerContainer TowerContainer;
-        
-        
+
+        private bool _isVacant;
+        private GameObject _target;
+
+        private void Init()
+        {
+            
+        }
+
+        private void Update()
+        {
+            if (_isVacant)
+            {
+                FindTarget();
+            }
+            else
+            {
+                if (_target != null)
+                {
+                    FireAction();
+                }
+            }
+        }
+
+        private void FireAction()
+        {
+            RotateTowerToTarget();
+            Fire();
+        }
+
+        private void RotateTowerToTarget()
+        {
+            transform.LookAt(_target.transform);
+        }
+
+        private void Fire()
+        {
+            var bullet = Instantiate(TowerContainer.Bullet);
+            bullet.transform.LookAt(_target.transform);
+        }
+
+        private void FindTarget()
+        {
+                var enemyes = TowerContainer.EnemySpawner.Enemyes;
+                _target = FindDistanceToEnemy.FindNearestEnemy(transform, enemyes);
+                if (_target != null)
+                {
+                    _isVacant = false;
+                }
+        }
     }
 }
