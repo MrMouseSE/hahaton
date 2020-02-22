70298a411461d19c89a4181bf7440a9b8fe9d0a5
Aleksey Savchenko <a.savchenko@kefirgames.com>
2020-02-22 17:19:09 +0400

    - add: start menu;

---
+++ Assets/fckingCODE/StartMenu.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class StartMenu : MonoBehaviour
+{
+    [SerializeField] private Button _startButton;
+
+    private void Awake()
+    {
+        _startButton.onClick.AddListener(() => { SceneManager.LoadScene("Start", LoadSceneMode.Single); });
+    }
+
+    private void OnDestroy()
+    {
+        _startButton.onClick.RemoveAllListeners();
+    }
+}
